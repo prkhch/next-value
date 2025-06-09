a4e68c7b7726a8047e21dad3fc8da933374fe0c2
prkhch <shjc4623@gmail.com>
2025-06-09 23:00:34 +0900

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -1,50 +1,29 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-int N, row[24];
-int mn = 987654321;
-
-void go(int now) {
-	if (now == N+1) {
-		int sum = 0;
-
-		// 현재 : 모든 경우의수 중 특정 행 중 True False가 겹치는지
-		for (int i = 1; i < (1 << N); i++) {
-			int cnt = 0;
-			for (int j = 0; j < N; j++) {
-				if (row[i] & (1 << j)) {
-					cnt++;
-				}
-			}
-			sum += min(cnt, N - cnt);
-		}
-		mn = min(sum, mn);
-		return;
-	}
-
-
-	go(now + 1);
-	row[now] = ~row[now];
-	go(now + 1);
-
-}
+int N, area[14], tmp;
+vector<vector<int>> v(14);
 
 int main() {
 
-	// 초기화 : 각 행에 T위치 정보를 하나의 숫자로 저장
 	cin >> N;
-	string str;
 	for (int i = 1; i <= N; i++) {
-		cin >> str;
-		for (int j = 0; j < N; j++) {
-			if (str[j] == 'T') {
-				row[i] |= (1 << j);
-			}
+		cin >> area[i];
+	}
+
+	for (int i = 1; i <= N; i++) {
+		int adj = 0; cin >> adj;
+		for (int j = 0; j < adj; j++) {
+			cin >> tmp;
+			v[i].push_back(tmp);
 		}
 	}
 
-	go(1);
-
-	cout << mn;
+	for (int i = 0; i < v.size(); i++) {
+		for (int j = 0; j < v[i].size(); j++) {
+			cout << v[i][j] << " ";
+		}
+		cout << "\n";
+	}
 
 }
