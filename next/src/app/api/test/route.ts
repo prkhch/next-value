88b29de1a9b2bf1dc14f2b2bba12a0c13ff2c60d
prkhch <shjc4623@gmail.com>
2025-07-01 20:03:43 +0900

    기능 : 넥스트제이에스에서 플라스크 연결

--- next/src/app/api/test/route.ts
+++ next/src/app/api/test/route.ts
@@ -1,5 +1,13 @@
+// https://nextjs.org/docs/app/api-reference/file-conventions/route
+// export async function GET() {
+//   return Response.json({
+//     message: "https://nextjs.org/docs/app/api-reference/file-conventions/route",
+//   });
+// }
+
 export async function GET() {
+  const flaskRes = await fetch("http://localhost:5000/api/flask/test");
   return Response.json({
-    message: "https://nextjs.org/docs/app/api-reference/file-conventions/route",
+    message: flaskRes.text(),
   });
 }
--- next/src/components/TestApiButton.tsx
+++ next/src/components/TestApiButton.tsx
@@ -2,18 +2,34 @@
 
 async function testApi() {
   const response = await fetch("/api/test");
+  console.log(response);
   const jsonData = await response.json();
   console.log(jsonData);
 }
 
+// CORS
+async function testApi2() {
+  const response = await fetch("http://localhost:5000/api/flask/test");
+  console.log(response);
+}
+
 export default function TestApiButton() {
   return (
-    <button
-      onClick={() => {
-        testApi();
-      }}
-    >
-      TestApiButton
-    </button>
+    <div>
+      <button
+        onClick={() => {
+          testApi();
+        }}
+      >
+        TestApiButton
+      </button>
+      <button
+        onClick={() => {
+          testApi2();
+        }}
+      >
+        TestApiButton2
+      </button>
+    </div>
   );
 }
