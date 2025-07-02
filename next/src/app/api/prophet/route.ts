93d9020210476938bfaac2900a86d5d7bf0006b5
prkhch <shjc4623@gmail.com>
2025-07-02 21:58:10 +0900

    기능 : 파일 업로드 및 서버 플라스크 전송 에이피아이

---
+++ next/src/app/api/prophet/route.ts
@@ -0,0 +1,30 @@
+// https://sangsangjin.tistory.com/29
+export async function POST(request: Request) {
+  const formData = await request.formData();
+  const flaskRes = await fetch("http://127.0.0.1:5000/api/flask/prophet", {
+    method: "POST",
+    body: formData,
+  });
+
+  const res = await flaskRes.json();
+  return Response.json({ res });
+}
+
+// json { file : , options : }
+/*
+    growth: "linear",
+    dfCap: 6,
+    dfFloor: 1.5,
+    ftCap: 6,
+    ftFloor: 1.5,
+    cpScale: 0.05,
+    cpList: [],
+    cpThreshold: 0.01,
+    periods: 365,
+    holidays: "none",
+    holidayScale: 10,
+    yearlyScale: "auto",
+    weeklyScale: "auto",
+    seasonMode: "additive",
+    seasonScale: 10,
+*/
--- next/src/components/TestApiButton.tsx
+++ next/src/components/TestApiButton.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 
 async function testApi() {
   const response = await fetch("/api/test");
@@ -13,7 +14,53 @@ async function testApi2() {
   console.log(response);
 }
 
+// 파일 전송 API
+async function sendFileToProphetAPI(formData: FormData) {
+  const response = await fetch("/api/prophet", {
+    method: "POST",
+    body: formData,
+  });
+  const result = await response.json();
+  console.log(result); // Base64 이미지 리턴 확인
+}
+
 export default function TestApiButton() {
+  // https://ethanmick.com/how-to-upload-a-file-in-next-js-13-app-directory/
+  // 브라우저 파일 업로드
+  // Upload, (csv, xls, xlsx)
+  const [file, setFile] = useState<File>();
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!file) return;
+    const data = new FormData();
+    data.set("file", file);
+    data.set(
+      "options",
+      // 플라스크에서 문자열형태로 받는중
+      JSON.stringify({
+        growth: "linear",
+        cpScale: 0.05,
+        cpList: [],
+        holidayScale: 10,
+        yearlyScale: "auto",
+        weeklyScale: "auto",
+        seasonMode: "additive",
+        seasonScale: 10,
+        holidays: "Korea",
+        dfCap: null,
+        dfFloor: null,
+        ftCap: null,
+        ftFloor: null,
+        periods: 30,
+        cpThreshold: 0.8,
+      })
+    );
+
+    if (data) {
+      sendFileToProphetAPI(data);
+    }
+  };
+
   return (
     <div>
       <button
@@ -30,6 +77,14 @@ export default function TestApiButton() {
       >
         TestApiButton2
       </button>
+      <form onSubmit={onSubmit}>
+        <input
+          type="file"
+          name="file"
+          onChange={(e) => setFile(e.target.files?.[0])}
+        />
+        <button type="submit">전송</button>
+      </form>
     </div>
   );
 }
