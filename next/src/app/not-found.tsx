51ffcc66af7e933ce8de8037486a8f7ab054a9c2
prkhch <shjc4623@gmail.com>
2025-07-27 19:06:11 +0900

    notfound페이지 클라이언트 컴포넌트 분리

--- next/src/app/not-found.tsx
+++ next/src/app/not-found.tsx
@@ -1,23 +1,9 @@
-"use client";
-import { useSearchParams } from "next/navigation";
-import Logo from "@/components/Logo";
-import SearchBar from "@/components/SearchBar";
+import NotFoundClient from "@/components/NotFoundClient";
 
 export default function NotFound() {
-  const searchParams = useSearchParams();
-  const symbol = searchParams.get("symbol");
-
   return (
     <main>
-      <Logo />
-
-      <SearchBar />
-      <div className="pt-10">
-        <div className="text-center font-thin text-6xl">404</div>
-        <div className="text-center font-thin">
-          No results for &apos;{symbol}&apos;
-        </div>
-      </div>
+      <NotFoundClient />
     </main>
   );
 }
---
+++ next/src/components/NotFoundClient.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import Logo from "@/components/Logo";
+import SearchBar from "@/components/SearchBar";
+
+export default function NotFoundClient() {
+  const searchParams = useSearchParams();
+  const symbol = searchParams.get("symbol");
+
+  return (
+    <>
+      <Logo />
+      <SearchBar />
+      <div className="pt-10">
+        <div className="text-center font-thin text-6xl">404</div>
+        <div className="text-center font-thin">
+          No results for &apos;{symbol}&apos;
+        </div>
+      </div>
+    </>
+  );
+}
