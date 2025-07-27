20119736f0ea395f1f6a3d1ec850599f21a40b8c
prkhch <shjc4623@gmail.com>
2025-07-27 09:45:29 +0900

    chore : tailwind 설치

--- next/src/app/chart/page.tsx
+++ next/src/app/chart/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 import PriceChart from "@/components/PriceChart";
 import { getClosingPrice } from "@/utils/fetchStock";
 import handleForamatDate from "@/utils/handleForamatDate";
+import SearchBar from "@/components/SearchBar";
 
 export default async function Chart({
   searchParams,
@@ -20,14 +21,19 @@ export default async function Chart({
     // 404 NotFound
     notFound();
   }
-
+  console.log(dataset);
   const formattedDates = dataset.res.chart.result[0].timestamp.map(
     (i: number) => handleForamatDate(i * 1000)
   );
 
   return (
     <main>
-      <h1>chart</h1>
+      <SearchBar />
+      <div>
+        {dataset.res.chart.result[0].meta.longName}(
+        {dataset.res.chart.result[0].meta.symbol})
+      </div>
+      <div>{dataset.res.chart.result[0].meta.currency}</div>
       <PriceChart
         labels={formattedDates}
         prices={dataset.res.chart.result[0].indicators.quote[0].close}
--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -3,7 +3,8 @@ import SearchBar from "@/components/SearchBar";
 export default async function Home() {
   return (
     <main>
-      <h1>TEST</h1>
+      <h1 className="text-3xl font-bold underline">TEST</h1>
+      <h1 className="underline">Hello world!</h1>
       <SearchBar />
     </main>
   );
