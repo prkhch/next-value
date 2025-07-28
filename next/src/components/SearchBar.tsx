4a0de00f15003fa9d9f6d8f09746b97431193c4b
prkhch <shjc4623@gmail.com>
2025-07-28 18:30:49 +0900

    fix : In HTML, <button> cannot be a descendant of <button>. This will cause a hydration error.

--- next/src/components/SearchBar.tsx
+++ next/src/components/SearchBar.tsx
@@ -51,7 +51,7 @@ const SearchBar = () => {
             type="search"
             id="search"
             className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search"
+            placeholder="Symbol"
             required
             value={input}
             onChange={(e) => setInput(e.target.value)}
